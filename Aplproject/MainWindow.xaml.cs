cc348fd240c469a486c856ab36c706025a173b3b
Aasesi <mikolajwilczynnski@gmail.com>
2023-12-12 21:46:27 +0100

    UI progress

--- Aplproject/MainWindow.xaml.cs
+++ Aplproject/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -35,9 +36,11 @@ namespace Aplproject
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BitmapImage bitmap;
         public MainWindow()
         {
             InitializeComponent();
+            bitmap = new BitmapImage();
         }
         private void loadImageButton_Click(object sender, RoutedEventArgs e)
         {
@@ -48,8 +51,33 @@ namespace Aplproject
             {
                 try
                 {
-                    // Load the selected image and display it in the Image control
-                    displayImage.Source = new System.Windows.Media.Imaging.BitmapImage(new Uri(openFileDialog.FileName));
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(openFileDialog.FileName);
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.EndInit();
+
+                    bitmap = convert_to_grayscale(bitmap);
+                    int stride = (bitmap.PixelWidth * bitmap.Format.BitsPerPixel + 7) / 8;
+                    byte[] pixelData = new byte[bitmap.PixelHeight * stride];
+                    bitmap.CopyPixels(pixelData, stride, 0);
+                    displayImage.Source = bitmap;
+
+                    string filePath = "pixelData.txt";
+
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        writer.WriteLine("Pixel Data:");
+                        writer.WriteLine(bitmap.Format.BitsPerPixel);
+                        
+
+                        for (int i = 0; i < pixelData.Length; i++)
+                        {
+
+                            writer.Write(pixelData[i] + " ");
+
+                        }
+                    }
+
                 }
                 catch (Exception ex)
                 {
@@ -57,6 +85,38 @@ namespace Aplproject
                 }
             }
 
+        }
+        private void cButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void asmButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private BitmapImage convert_to_grayscale(BitmapImage bitmap)
+        {
+            FormatConvertedBitmap newFormatedBitmapSource = new FormatConvertedBitmap();
+            newFormatedBitmapSource.BeginInit();
+            newFormatedBitmapSource.Source = bitmap;
+            newFormatedBitmapSource.DestinationFormat = PixelFormats.Gray8;
+            newFormatedBitmapSource.EndInit();
+
+
+
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            MemoryStream memoryStream = new MemoryStream();
+            encoder.Frames.Add(BitmapFrame.Create(newFormatedBitmapSource));
+            encoder.Save(memoryStream);
+
+            BitmapImage grayscaleBitmap = new BitmapImage();
+            grayscaleBitmap.BeginInit();
+            grayscaleBitmap.StreamSource = memoryStream;
+            grayscaleBitmap.EndInit();
+            grayscaleBitmap.CacheOption = BitmapCacheOption.OnLoad;
+
+            return grayscaleBitmap;
         }
     }
 }
